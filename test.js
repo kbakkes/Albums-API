c43bbcbb84bf1f5f8a8938b5298df132f1639d8b
kbakkes <karim.bakkes@gmail.com>
2018-12-11 20:05:59 +0100

    Add mock data file

---
+++ test.js
@@ -0,0 +1,40 @@
+db.albums.insert(
+    [
+        {
+            name:  'Culture',
+            artist: 'Migos',
+            year:   '2017',
+            genre:  'Hip-Hop'
+        },
+        {
+            name:  'Culture 2',
+            artist: 'Migos',
+            year:   '2018',
+            genre:  'Hip-Hop'
+        },
+        {
+            name:  'Sremmlife 2',
+            artist: 'Rae Sremmurd',
+            year:   '2017',
+            genre:  'Hip-Hop'
+        },
+        {
+            name:  'Starboy',
+            artist: 'The Weeknd',
+            year:   '2017',
+            genre:  'RnB'
+        },
+        {
+            name:  'Championships',
+            artist: 'Meek Mill',
+            year:   '2018',
+            genre:  'Hip-Hop'
+        },
+        {
+            name:  'Dummyboy ',
+            artist: '6ix9ine',
+            year:   '2018',
+            genre:  'Hip-Hop'
+        }
+    ]
+);
